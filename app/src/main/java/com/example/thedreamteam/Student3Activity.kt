9ee69a0c8abb89a820c35b3eb93c55e280078c25
Bejay Bacilonia <>
2026-01-15 15:06:47 +0800

    Student Screen 3

--- app/src/main/java/com/example/thedreamteam/Student3Activity.kt
+++ app/src/main/java/com/example/thedreamteam/Student3Activity.kt
@@ -1,1 +1,20 @@
-package com.example.thedreamteam
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BioScreen3() {
+
+    val name = "Bejay L. Bacilonia"
+    val section = "3IT-A"
+    val age = "20"
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(text = "Name: $name")
+        Text(text = "Section: $section")
+        Text(text = "Age: $age")
+    }
+}
