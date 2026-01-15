1ad62f2a1b6a9a8d92f15d4d9651427e840a4274
Bejay Bacilonia <>
2026-01-15 15:17:07 +0800

    Student Screen 3

--- app/src/main/java/com/example/thedreamteam/Student3Activity.kt
+++ app/src/main/java/com/example/thedreamteam/Student3Activity.kt
@@ -1,20 +1,51 @@
+package com.example.thedreamteam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class Student3Activity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Student3Screen()
+        }
+    }
+}
 
 @Composable
-fun BioScreen3() {
+fun Student3Screen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-    val name = "Bejay L. Bacilonia"
-    val section = "3IT-A"
-    val age = "20"
+        Image(
+            painter = painterResource(id = R.drawable.jayb),
+            contentDescription = "Bacilonia' photo",
+            modifier = Modifier.size(150.dp)
+        )
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "Name: $name")
-        Text(text = "Section: $section")
-        Text(text = "Age: $age")
+        Text(
+            text = "Bejay Bacilonia",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+
     }
 }
