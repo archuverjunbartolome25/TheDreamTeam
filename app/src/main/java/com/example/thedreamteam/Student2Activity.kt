11dd9bd92bca21918727c4e654a03421b2464003
rgngrc <ayesreginagrace0808@gmail.com>
2026-01-15 15:07:57 +0800

    Add student 2's photo and name

--- app/src/main/java/com/example/thedreamteam/Student2Activity.kt
+++ app/src/main/java/com/example/thedreamteam/Student2Activity.kt
@@ -1,2 +1,52 @@
 package com.example.thedreamteam
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class Student2Activity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Student2Screen()
+        }
+    }
+}
+
+@Composable
+fun Student2Screen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.student2),
+            contentDescription = "Ayes' photo",
+            modifier = Modifier.size(150.dp)
+        )
+
+        Text(
+            text = "Regina Grace Ayes",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+}
+
