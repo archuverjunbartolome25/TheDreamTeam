a271483920cf1c464021e701a002c6eeb4677e77
rui101905 <louieandrew.magahis@gmail.com>
2026-01-15 15:32:26 +0800

    Student 4 bio_Screen commit

--- app/src/main/java/com/example/thedreamteam/Student4Activity.kt
+++ app/src/main/java/com/example/thedreamteam/Student4Activity.kt
@@ -1,2 +1,54 @@
 package com.example.thedreamteam
 
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class Student4Activity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Student4Screen()
+        }
+    }
+}
+
+@Composable
+fun Student4Screen() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.student4),
+            contentDescription = "Balgos Photo",
+            modifier = Modifier.size(150.dp)
+        )
+
+        Text(
+            text = "Carl Lourence A. Balgos",
+            fontSize =  24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+
+
+
